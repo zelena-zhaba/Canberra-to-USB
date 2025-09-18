86943da8db164917711231ac3c8c31d59de3243a
zelena-zhaba <zelena_zhaba@yahoo.com>
2025-09-18 19:04:43 +0300

    solid-slave

--- Canberra-to-USB.c
+++ Canberra-to-USB.c
@@ -17,7 +17,6 @@
 
 #define ADC_COUNT 14
 #define BASE_PIN 2
-#define ENC_PIN 21
 
 #include "input.pio.h"
 
@@ -25,23 +24,21 @@ int main(void) {
     stdio_init_all();
     sleep_ms(150); 
 
+    // ENC active-low to enable ADC 
+    gpio_init(21);
+    gpio_set_dir(21, GPIO_OUT);
+    gpio_put(21, 0);
+
     // Level shifter enable 
     gpio_init(22);
     gpio_set_dir(22, GPIO_OUT);
     gpio_put(22, 1);
 
-    // ENC active-low: drive low permanently
-    gpio_init(ENC_PIN);
-    gpio_set_dir(ENC_PIN, GPIO_OUT);
-    gpio_put(ENC_PIN, 0);
-
     // Load PIO program and claim a state machine
     PIO pio = pio0;
     int sm = pio_claim_unused_sm(pio, true);
     uint offset = pio_add_program(pio, &input_program);
-    input_program_init(pio, sm, offset, BASE_PIN);
-    // Program's init enabled SM already; ensure it's running
-    pio_sm_set_enabled(pio, sm, true);
+    input_program_init(pio, sm, offset);
 
     const uint16_t mask = (1u << ADC_COUNT) - 1u;
     while (true) {
